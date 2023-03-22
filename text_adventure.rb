44de01dc731354db766021330c608fa9f0aac0ad
Barney <B.Wilton1993@gmail.com>
2023-03-22 13:10:33 +0000

    Created text adventure file and outlined requirements

---
+++ text_adventure.rb
@@ -0,0 +1,5 @@
+# User starts facing forwards
+# Can move right, left, or forward
+# If move right - die, to goblin
+# If move left - die, to werewolf
+# If move forwards - live, must do twice to win
