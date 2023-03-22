f217fc28fcabd141b2f4ade90f5d13dd8a7fe9f5
Barney <B.Wilton1993@gmail.com>
2023-03-22 13:07:33 +0000

    Created program that shouts usernames back if they start with S

--- name_beginning_with_s.rb
+++ name_beginning_with_s.rb
@@ -1,4 +1,13 @@
 # Greet user
+puts "Greetings user! What is your name?"
+
 # Get user input - name
-# If user name starts with S, shout at user
+user_name = gets.chomp().downcase
+
+# If user name starts with S, shout the users name back at them (all capitals)
 # Otherwise greet user with friendly greeting
+if user_name[0] == 's'
+  puts "#{user_name.upcase}"
+else
+  puts "Hi, #{user_name.capitalize}"
+end
